dfc0a8b1061c433c9f084df5497c18ebbc675a54
jgcruz81 <juangcruz81@gmail.com>
2025-03-17 11:17:45 -0400

    added page & hello

---
+++ app/components/hello.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+function Hello() {
+    console.log("I am Client Component")
+
+    return (
+        <div>
+            <h1>
+                Hello
+            </h1>
+        </div>
+    );
+}
+
+export default Hello;
+
